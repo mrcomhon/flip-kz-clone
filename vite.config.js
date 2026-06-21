7204079b98ae17f0a39cba3a533d3f31b8f4912c
Umar I <umarimanmagzam@gmail.com>
2026-06-21 15:51:45 +0500

    chore: add and configure vite-plugin-svgr for svg components

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr(),
+  ],
 
   // 1. Создаем алиас (шорткат) "@", который всегда ссылается на папку src
   resolve: {
