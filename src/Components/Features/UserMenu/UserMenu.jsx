562f64a4063c276c0ceb8717087446b95c5970e1
Umar I <umarimanmagzam@gmail.com>
2026-08-22 13:00:29 +0500

    refactor: add new folder for favorite

--- src/Components/Features/UserMenu/UserMenu.jsx
+++ src/Components/Features/UserMenu/UserMenu.jsx
@@ -1,6 +1,6 @@
 import styles from './UserMenu.module.scss'
 import Cart from "@/components/cart/Cart";
-import Favorite from "@/components/Features/Favorite/Favorite";
+import Favorite from "@/components/favorite/Favorite";
 import LangSwitcher from "@/components/Features/LanguageSwitcher/LangSwitcher";
 import Auth from "@/components/auth/Auth";
 
