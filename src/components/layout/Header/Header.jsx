d4d9238fdf388a29c7bfe655f716207da9f2ff30
Umar I <umarimanmagzam@gmail.com>
2026-08-24 11:59:27 +0500

    refactor: change style container to Container component

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -2,30 +2,35 @@ import { LogoWrapper } from "@/components/header/LogoWrapper";
 import { SearchBar } from "@/components/header/SearchBar";
 import { UserMenu } from "@/components/header/UserMenu";
 import styles from "./Header.module.scss";
+import { Container } from "../Container";
 
 export function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.desktopHeader}>
-        <nav className={`${styles.navigation} container`}>
+        <Container>
+        <nav className={styles.navigation}>
           <LogoWrapper />
           <SearchBar />
           <UserMenu />
         </nav>
+        </Container>
       </div>
       <div className={styles.mobileHeader}>
-        <nav className={`${styles.mobileNavigation} container`}>
+        <Container>
+        <nav className={styles.mobileNavigation}>
           <div className={styles.headerTop}>
             <LogoWrapper LocationClass="hide-tab" />
             <UserMenu />
           </div>
           <SearchBar />
         </nav>
+        </Container>
           <LogoWrapper
             LocationClass="visible-tablet"
             LogoClass="hide-tab visually-hidden"
             BurgerClass="hide-tab visually-hidden"
-          />
+            />
       </div>
     </header>
   )
