8c767313646f083088adcb5db3177ad67db1f5cd
Umar I <umarimanmagzam@gmail.com>
2026-08-26 11:23:38 +0500

    refactor: correct name for type, import type, formatter

--- src/components/layout/Container/Container.tsx
+++ src/components/layout/Container/Container.tsx
@@ -1,12 +1,12 @@
 import clsx from "clsx";
-import { ReactNode } from 'react';
-import styles from "./Container.module.scss"; 
+import type { ReactNode } from "react";
+import styles from "./Container.module.scss";
 
-type containerProps = {
-    children: ReactNode,
-    className?: string;
-}
+type ContainerProps = {
+  children: ReactNode;
+  className?: string;
+};
 
-export function Container({children, className}: containerProps) {
-    return <div className={clsx(styles.container, className)}>{children}</div>;
+export function Container({ children, className }: ContainerProps) {
+  return <div className={clsx(styles.container, className)}>{children}</div>;
 }
