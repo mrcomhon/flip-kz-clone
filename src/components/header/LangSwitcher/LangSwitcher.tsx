6438540901dec160947ad19f5cae9f0d26407d8c
Umar I <umarimanmagzam@gmail.com>
2026-08-27 10:50:23 +0500

    refactor(LangSwitcher): migrate to TypeScript

--- src/components/header/LangSwitcher/LangSwitcher.tsx
+++ src/components/header/LangSwitcher/LangSwitcher.tsx
@@ -1,25 +1,19 @@
 import clsx from "clsx";
 import styles from "./LangSwitcher.module.scss";
 
-export function LangSwitcher({className}) {
+type LangSwitcherProps = {
+  className?: string;
+};
+
+export function LangSwitcher({ className }: LangSwitcherProps) {
   return (
     <div className={clsx(styles.langSwitcherWrapper, className)}>
       <div className={styles.langSwitcher}>
-        <input
-          id="kz"
-          name="lang"
-          value="kz"
-          type="radio"
-        />
+        <input id="kz" name="lang" value="kz" type="radio" />
         <label htmlFor="kz">Қаз</label>
-        <input
-          id="ru"
-          name="lang"
-          value="ru"
-          type="radio"
-        />
+        <input id="ru" name="lang" value="ru" type="radio" />
         <label htmlFor="ru">Рус</label>
       </div>
     </div>
-  )
+  );
 }
