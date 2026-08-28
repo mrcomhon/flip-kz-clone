3d1d724f7efda6c93611a19873a931ad8ede7765
Umar I <umarimanmagzam@gmail.com>
2026-08-28 15:58:50 +0500

    refactor(header): render menu items with map

--- src/components/header/MenuList/MenuList.tsx
+++ src/components/header/MenuList/MenuList.tsx
@@ -1,70 +1,95 @@
 import styles from "./MenuList.module.scss";
-import { MdWindow } from "react-icons/md";
-import { BsCurrencyDollar, BsFire } from "react-icons/bs";
-import { BsFillCreditCardFill } from "react-icons/bs";
+import type { ReactElement } from "react";
+import { MdWindow, MdHelp } from "react-icons/md";
+import { BsCurrencyDollar, BsFire, BsFillCreditCardFill } from "react-icons/bs";
 import { ImLocation } from "react-icons/im";
 import { IoCart } from "react-icons/io5";
 import { IoMdHeart } from "react-icons/io";
-import { MdHelp } from "react-icons/md";
 import { BiLogoTelegram } from "react-icons/bi";
+import clsx from "clsx";
+
+type MenuListItem = {
+  id: string;
+  href: string;
+  icon: ReactElement;
+  label: string;
+  hasDivider?: boolean;
+  isLocation?: boolean;
+};
 
 export function MenuList() {
+  const menuItems: MenuListItem[] = [
+    {
+      id: "catalog",
+      href: "#",
+      icon: <MdWindow />,
+      label: "Каталог",
+    },
+    {
+      id: "discounts",
+      href: "#",
+      icon: <BsFire />,
+      label: "Скидки и акции",
+    },
+    {
+      id: "gifts",
+      href: "#",
+      hasDivider: true,
+      icon: <BsFillCreditCardFill />,
+      label: "Подарочные карты",
+    },
+    {
+      id: "city",
+      href: "#",
+      isLocation: true,
+      icon: <ImLocation />,
+      label: "Алматы",
+    },
+    {
+      id: "cart",
+      href: "#",
+      hasDivider: true,
+      icon: <IoCart />,
+      label: "Корзина",
+    },
+    {
+      id: "favorite",
+      href: "#",
+      hasDivider: true,
+      icon: <IoMdHeart />,
+      label: "Избранное",
+    },
+    {
+      id: "help",
+      href: "#",
+      icon: <MdHelp />,
+      label: "Помощь",
+    },
+    {
+      id: "delivery",
+      href: "#",
+      icon: <BiLogoTelegram />,
+      label: "Способы доставки",
+    },
+    {
+      id: "payment",
+      href: "#",
+      hasDivider: true,
+      icon: <BsCurrencyDollar />,
+      label: "Способы оплаты",
+    },
+  ];
+
   return (
     <ul className={styles.list}>
-      <li>
-        <a href="#" className={styles.item}>
-          <MdWindow />
-          <p>Каталог</p>
-        </a>
-      </li>
-      <li>
-        <a href="#" className={styles.item}>
-          <BsFire />
-          <p>Скидки и акции</p>
-        </a>
-      </li>
-      <li className={styles.last}>
-        <a href="#" className={styles.item}>
-          <BsFillCreditCardFill />
-          <p>Подарочные карты</p>
-        </a>
-      </li>
-      <li>
-        <a href="#" className={styles.item}>
-          <ImLocation />
-          <p style={{ color: "var(--color-blue-light)" }}>Алматы</p>
-        </a>
-      </li>
-      <li className={styles.last}>
-        <a href="#" className={styles.item}>
-          <IoCart />
-          <p>Корзина</p>
-        </a>
-      </li>
-      <li className={styles.last}>
-        <a href="#" className={styles.item}>
-          <IoMdHeart />
-          <p>Избранное</p>
-        </a>
-      </li>
-      <li>
-        <a href="#" className={styles.item}>
-          <MdHelp />
-          <p>Помощь</p>
-        </a>
-      </li>
-      <li>
-        <a href="#" className={styles.item}>
-          <BiLogoTelegram />
-          <p>Способы доставки</p>
-        </a>
-      </li>
-      <li className={styles.last}>
-        <a href="#" className={styles.item}>
-          <BsCurrencyDollar />
-          <p>Способы оплаты</p>
-        </a>
-      </li>
+      {menuItems.map((item) => (
+        <li key={item.id} className={clsx(item.hasDivider && styles.last)}>
+          <a href={item.href} className={styles.item}>
+            {item.icon}
+            <p className={clsx(item.isLocation && styles.city)}>{item.label}</p>
+          </a>
+        </li>
+      ))}
     </ul>
   );
 }
