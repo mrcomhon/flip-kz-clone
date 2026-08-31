b1f6d96dc2815f8418d130f8e066481e3040cedf
Umar I <umarimanmagzam@gmail.com>
2026-08-31 11:32:52 +0500

    chore(i18n): type translation resources

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -4,11 +4,16 @@ import { initReactI18next } from "react-i18next";
 import ru from "./locales/ru";
 import kk from "./locales/kk";
 
+export const defaultNS = "translation";
+
+export const resources = {
+  ru: { translation: ru },
+  kk: { translation: kk },
+};
+
 i18n.use(initReactI18next).init({
-  resources: {
-    ru: { translation: ru },
-    kk: { translation: kk },
-  },
+  resources,
+  defaultNS,
   lng: "ru",
   fallbackLng: "ru",
   supportedLngs: ["kk", "ru"],
