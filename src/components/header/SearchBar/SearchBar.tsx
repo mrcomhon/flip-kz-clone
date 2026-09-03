8345539445ca8ad671b067bada5e0c88fc602834
Umar I <umarimanmagzam@gmail.com>
2026-09-03 13:48:33 +0500

    feat(search): add live product filtering

--- src/components/header/SearchBar/SearchBar.tsx
+++ src/components/header/SearchBar/SearchBar.tsx
@@ -2,18 +2,29 @@ import { useTranslation } from "react-i18next";
 import SearchIcon from "@/assets/icons/search.svg?react";
 import styles from "./SearchBar.module.scss";
 
-export function SearchBar() {
+type SearchBarProps = {
+  value: string;
+  onSearchChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export function SearchBar({ value, onSearchChange }: SearchBarProps) {
   const { t } = useTranslation();
 
   return (
     <div className={styles.searchBar}>
-      <form action="/search" name="search" className={styles.searchForm}>
+      <form
+        onSubmit={(event) => event.preventDefault()}
+        name="search"
+        className={styles.searchForm}
+      >
         <input
           className={styles.searchInput}
           type="search"
-          id="search"
           name="search"
           placeholder={t("header.search.placeholder")}
+          aria-label={t("header.search.placeholder")}
+          value={value}
+          onChange={onSearchChange}
         />
         <button
           className={styles.buttonSearch}
