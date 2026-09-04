98f4e328e91527511af7ac5a3f362365d2c7583a
Umar I <umarimanmagzam@gmail.com>
2026-09-04 12:22:55 +0500

    fix(a11y): improve header link accessibility

--- src/components/auth/MobileAuth/MobileAuth.tsx
+++ src/components/auth/MobileAuth/MobileAuth.tsx
@@ -1,11 +1,18 @@
 import AuthIcon from "@/assets/icons/account.svg?react";
 import styles from "./MobileAuth.module.scss";
+import { useTranslation } from "react-i18next";
 
 export function MobileAuth() {
+  const { t } = useTranslation();
+
   return (
     <div className={styles.mobileAuth}>
-      <a href="#" className={styles.authIcon}>
-        <AuthIcon className={styles.icon} />
+      <a
+        href="#"
+        className={styles.authIcon}
+        aria-label={t("header.auth.login")}
+      >
+        <AuthIcon className={styles.icon} aria-hidden="true" />
       </a>
     </div>
   );
